68fdec402a4ae6274ec297405cdbbabdc6866795
Cnix24 <calvinattl24@gmail.com>
2019-07-17 15:59:13 -0600

    Update

---
+++ Step115/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step115
+{
+    class MethodClass
+    {
+        public int MethodX(int x, int y=0)
+        {
+            int answer = x + y;
+            return answer;
+        }
+    }
+}
---
+++ Step115/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Step115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            MethodClass method1 = new MethodClass();
+            Console.WriteLine("Please enter a number: ");
+            string xString = Console.ReadLine();
+
+            Console.WriteLine("Enter a second number, or leave blank: ");
+            string yString = Console.ReadLine();
+            try
+            {
+                y = Convert.ToInt32(yString);
+            }
+            catch
+            {
+                y = 0;
+            }
+            
+            x = Convert.ToInt32(xString);
+           
+
+            int answer = method1.MethodX(x, y);
+            Console.WriteLine("Your two numbers combined equals: "+answer);
+
+            Console.ReadLine();
+        }
+    }
+}
